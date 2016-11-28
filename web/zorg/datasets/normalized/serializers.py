1bd41a0bcd5eb929fe0370d5294ae387f9f414cc
Kfir Breger <kfir@cubicalwave.com>
2016-11-28 08:32:26 +0100

    Adding normilzed docs, serializes and events

---
+++ web/zorg/datasets/normalized/serializers.py
@@ -0,0 +1,48 @@
+# Packages
+from rest_framework import serializers
+# Projects
+import events
+import models
+
+
+class OrganisatieSerialiserCustom(serializers.Serilalizer):
+    guid = serializers.CharField()
+    naam = serializers.CharField()
+    beschrijving = serializers.CharField()
+    afdeling = serializers.CharField()
+    contact = serializers.JsonField()  # for tele, fax, emai, www etc.
+
+
+class OrganisatieSerialiser(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        # Creating the guid
+        guid = events.guid_from_id(validated_data['id'])
+
+        # There can bew two cases in which create can be made:
+        # 1. There is no previous entry
+        # 2. The laatste event was een delete
+        prev_events = models.OrganisatieEventLog.objects.filter(
+                      guid=guid).order_by('sequence')
+        if len(prev_events) == 0:
+            sequence = 0
+        elif prev_events[-1].action == 'D':
+            sequence = prev_events[-1].sequence + 1
+        else:
+            # Does not math creation rule
+            return False  # @TODO is this enough?
+        sequence = prev_events[-1].sequence + 1 if len(prev_events) > 0 and 
+
+        event = models.OrganisatieEventLog(
+            guid=guid,
+            sequence=sequence,
+            action='C',
+            data=validated_data
+        )
+        return event.save()
+
+    def update(self, instance, validated_data):
+        pass
+
+    class Meta(object):
+        model = Organisatie
