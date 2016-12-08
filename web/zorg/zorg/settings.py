50c0a465c20945f86218fb95cf863f8184db76e6
Kfir Breger <kfir@cubicalwave.com>
2016-12-08 11:15:51 +0100

    Mapping static url correctly

--- web/zorg/zorg/settings.py
+++ web/zorg/zorg/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/zorg/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
 
 # Elastic
