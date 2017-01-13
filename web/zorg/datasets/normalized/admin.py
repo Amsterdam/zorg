575fadf64b7acfb6ee6ae0a4a03b638b0a024a5b
Kfir Breger <kfir@cubicalwave.com>
2017-01-13 12:14:16 +0100

    Adding admin class to normalized data

---
+++ web/zorg/datasets/normalized/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+form models import Locatie, Organisatie, Activiteit
+
+
+class LocatieAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrganisatieAdmin(admin.ModelAdmin):
+    pass
+
+
+class ActiviteitAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Locatie, LocatieAdmin)
+admin.site.register(Organisatie, OrganisatieAdmin)
+admin.site.register(Activiteit, ActiviteitAdmin)
