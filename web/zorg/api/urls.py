cc48f0f1dfda737948abe4009cd229c45ce4c51f
Thijs Creemers <thijs.creemers@gmail.com>
2017-04-12 12:02:28 +0200

    changed openapi url

--- web/zorg/api/urls.py
+++ web/zorg/api/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     url(r'^zorg/zoek/activiteit/$', ZoekApiView.as_view(), {'search_for': 'activiteit'}),
     url(r'^zorg/zoek/locatie/$', ZoekApiView.as_view(), {'search_for': 'locatie'}),
     url(r'^zorg/zoek/organisatie/$', ZoekApiView.as_view(), {'search_for': 'organisatie'}),
-    url(r'zorg/zoek/geo/$', GeoZoekView.as_view()),
-    url(r'zorg/zoek/thema/$', TermsZoekView.as_view()),
-    url(r'zorg/zoek/$', ZoekApiView.as_view()),
-    url(r'openapi.json', OpenApiView.as_view())
+    url(r'^zorg/zoek/geo/$', GeoZoekView.as_view()),
+    url(r'^zorg/zoek/thema/$', TermsZoekView.as_view()),
+    url(r'^zorg/zoek/$', ZoekApiView.as_view()),
+    url(r'^zorg/openapi.yml', OpenApiView.as_view())
 
 ]
