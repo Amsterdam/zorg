04a085b40c3687f9a7db0b05e29227d7e720c462
Evert Lammerts <evert@lucipher.net>
2017-06-09 12:06:04 +0200

    simplified search

--- web/zorg/api/views.py
+++ web/zorg/api/views.py
@@ -3,28 +3,39 @@
 .. todo::
     Reorganize this code and implement content negotiation.
 """
-# Python
 import functools
-import json
 import logging
 import os
+import sys
 
-# Packages
-from django.conf import settings
-from django.http import HttpResponse
-from django.views import View
+from django.http import HttpResponse, HttpResponseServerError
 from django.views.decorators.http import require_GET
-from elasticsearch import Elasticsearch, RequestError
 
 # Project
 from datasets.normalized import elastic
 
-log = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
 @require_GET
 def search(request, doctype=None):
-    pass
+    """Perform a search"""
+    queryparams = request.GET
+    # this is ugly!! is it lazy? because in that case I would rather create a
+    # QueryDict instance from META['query_string']
+    q = queryparams.get('query')
+    lon, lat = queryparams.get('lon'), queryparams.get('lat')
+    lonlat = lon and lat and (lon, lat)
+    try:
+        return HttpResponse(
+            elastic.search(q, doctype, lonlat),
+            content_type='application/json')
+    except elastic.SearchError:
+        return HttpResponseServerError()
+    except:
+        _logger.fatal(
+            'Unexpected exception while searching', exc_info=sys.exc_info())
+        return HttpResponseServerError()
 
 
 @require_GET
@@ -45,73 +56,3 @@ def _swagger_yml():
     path = '{}/openapi.yml'.format(os.path.dirname(os.path.abspath(__file__)))
     with open(path) as file:
         return file.read()
-
-
-class ZoekApiView(View):
-    zoek_functie = queries.zorg_Q
-
-    def __init__(self, *args, **kwargs):
-        super(ZoekApiView, self).__init__(*args, **kwargs)
-        self.elastic = Elasticsearch(
-            hosts=settings.ELASTIC_SEARCH_HOSTS, retry_on_timeout=True,
-            refresh=True
-        )
-
-    def get_query(self):
-        """
-        Returns the information needed by the query
-        Naive implementation expects a query parameter in GET or POST
-        and returns that as a string. This can be overwritten to provide
-        more complex query needs.
-
-        If nothing is found returns an emptuy string. This is in line with
-        elastic behavior of suppling all the items in case of search with
-        no given query
-        """
-        request_dict = getattr(self.request, self.request.method)
-        return request_dict.get('query', '')
-
-    def search_elastic(self, search_for: str, query: str) -> HttpResponse:
-        query = self.__class__.zoek_functie(self.get_query(), search_for)
-        query['size'] = 1000
-        # Perform search
-        try:
-            response = self.elastic.search(
-                index=settings.ELASTIC_INDEX,
-                body=query
-            )
-        except RequestError as exp:
-            log.error(f'{exp!r}')
-            return HttpResponse(f"Search encounterd an error in the request, {exp}")
-        except Exception as exp:
-            log.error(f'{exp!r}')
-            return HttpResponse("Error processing request")
-        return HttpResponse(json.dumps(response))
-
-    def get(self, *args, **kwargs):
-        return self.search_elastic(kwargs.get('search_for', None), self.request.GET.get('query', ''))
-
-    def post(self, *args, **kwargs):
-        return self.search_elastic(kwargs.get('search_for', None), self.request.POST.get('query', ''))
-
-
-class TermsZoekView(ZoekApiView):
-    zoek_functie = queries.terms_Q
-
-
-class GeoZoekView(ZoekApiView):
-    zoek_functie = queries.geo_Q
-
-    def get_query(self):
-        """
-        Overwriting to support lat / lon / text
-        @TODO do some cleanup, maybe?
-        """
-        request_dict = getattr(self.request, self.request.method)
-        return request_dict
-
-
-class OpenApiView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse(open(f'{os.path.dirname(os.path.abspath(__file__))}/openapi.yml').read(),
-                            content_type='application/yaml')
--- web/zorg/datasets/normalized/elastic.py
+++ web/zorg/datasets/normalized/elastic.py
@@ -1,15 +1,15 @@
-# Python
 import functools
 import json
 import logging
-# Packages
+import sys
+
 from django.conf import settings
-from elasticsearch import Elasticsearch, RequestError
+from elasticsearch import Elasticsearch
 
-log = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
-class QueryError(Exception):
+class SearchError(Exception):
     """Error that indicates to the user of this module that something went
     wrong while searching."""
 
@@ -30,145 +30,38 @@ def _elasticsearch():
     )
 
 
-def _search(query):
-    """Fire queries at Elasticsearch.
-    """
-    query['size'] = 1000
-
-    response = _elasticsearch().search(
-        index=settings.ELASTIC_INDEX,
-        body=query
-    )
-    return json.dumps(response)
-
-
-def search(q='', doctype=None, latlong=None):
-    fields = ["naam^1.5", "beschrijving"]
-    should = [{"multi_match": {"query": t, "fields": fields}} for t in q.split()]
-    filter = (doctype and {"type": {"value": doctype}}) or None
-    if latlong:
-        lat, long = latlong
-
-
-def zorg_Q(query_string: dict, doc_type: str) -> dict:
-    if doc_type:
-        q = {
-            "query": {
-                "bool": {
-                    "must": {
-                        "multi_match": {
-                            "query": query_string,
-                            "fields": ["naam^1.5", "beschrijving"]
-                        }
-                    },
-                    "filter": {
-                        "type": {"value": doc_type}
-                    }
-                }
+def search(q='', doctype=None, lonlat=None):
+    """Generate and fire an Elastic query"""
+    fields = ['naam^1.5', 'beschrijving']
+    should = [
+        {'multi_match': {'query': t, 'fields': fields}} for t in q.split()
+    ]
+    searchfilter = (doctype and {'type': {'value': doctype}}) or {}
+    sort = ['_score']
+    if lonlat:
+        sort.insert(0, {
+            '_geo_distance': {
+                'pin.location': lonlat,
+                'order': 'asc',
+                'unit': 'km'
             }
-        }
-    else:
-        q = {
-            "query": {
-                "multi_match": {
-                    "query": query_string,
-                    "fields": ["naam^1.5", "beschrijving"]
-                }
-            }
-        }
-    return q
-
-
-def terms_Q(query_string: str, doc_type: str) -> dict:
-    terms = query_string.split(' ')
-    q = {
-        "query": {
-            "bool": {
-                "should": [
-                    {
-                        "terms": {
-                            "naam": terms
-                        }
-                    },
-                    {
-                        "terms": {
-                            "beschrijving": terms
-                        }
-                    }
-                ],
-                "minimum_should_match": 1
-            },
-        }
-    }
-
-    return q
-
-
-def geo_Q(query: dict, doc_type=None) -> dict:
-    """
-    Perform a geospatial search.
-    The query parameter contains the needed information for the query.
-    There are two required keys: 'lat' and 'lon'
-    An optional 'text' key can given to also
-    query name and/or description
-    """
-    default_distance = '2km'
-    # Allowing for text filtering in query, if needed
-    try:
-        match = {
-            "multi_match": {
-                "query": query['text'],
-                "fields": ["naam^1.5", "beschrijving"]
-            }
-        }
-    except KeyError:
-        match = {'match_all': {}}
-
-    geo_distance = {
-        "geo_distance": {
-            "distance": default_distance,
-            "locatie.centroid": {
-                "lat": query['lat'],
-                "lon": query['lon']
-            }
-        }
-    }
-
-    # Sorting by geo-distance add it to the results
-    # Since sorting by _score is the intention, sorting first
-    # by score and then by distance
-    q = {
-        "query": {
-            "bool": {
-                "must": [
-                    match,
-                    {
-                        "geo_distance": {
-                            "distance": default_distance,
-                            "locatie.centroid": {
-                                "lat": query['lat'],
-                                "lon": query['lon']
-                            }
-                        }
-                    }
-                ]
-            }
+        })
+    query = {
+        'query': {
+            'should': should,
+            'filter': searchfilter
         },
-        "sort": [
-            "_score",
-            {
-                "_geo_distance": {
-                    "locatie.centroid": {
-                        "lat": query['lat'],
-                        "lon": query['lon']
-                    },
-                    "order": "asc",
-                    "unit": "km",
-                    "mode": "min",
-                    "distance_type": "plane"
-                }
-            }
-        ]
+        'sort': sort,
+        'size': 1000
     }
 
-    return q
+    try:
+        response = _elasticsearch().search(
+            index=settings.ELASTIC_INDEX,
+            body=query
+        )
+    except Exception as e:
+        _logger.critical('Exception while searching', exc_info=sys.exc_info())
+        raise SearchError() from e
+
+    return json.dumps(response)
